87f4e3278e77d47e90ec3393e5e85209b334d6be
Utkarsh Patil <utkarsh2305@gmail.com>
2017-11-15 00:50:12 +0530

    Add spell checker and auto complete

--- Implementation.py
+++ Implementation.py
@@ -2,9 +2,9 @@ from Radix import *
 
 def main():
     R = RadixTree()
-    names=['hey','wow','hi','tailor','tailored','tail','tailoreing','he','wonderful','hello']
+    names = ['hey','wow','hi','tailor','tailored','tail','tailoring','he','wonderful','hello']
     for name in names:
-        R.insert(R.root,name)
+        R.insert(R.root, name)
     print(R.root.children)
     print(R.root.children['h'].children['e'].children)
     print(R.root.children['tail'].children)
@@ -15,19 +15,9 @@ def main():
     print(R.search(R.root, "he"))
     print(R.search(R.root, "tail"))
     print(R.search(R.root, "tailored"))
-    print(R.printTree(names,0))
-    f=open('small.txt')
-    words=f.readlines()
-    words=[line[:-1] for line in words]
-    for a in words:
-        R.insert(R.root,a)
-    w=input("Enter a word to check its validity: ")
-    if R.search(R.root,w):
-        print("Correct spelling")
-    else:
-        print("Incorrect spelling")
+    print()
+    R.print_tree(R.root, "")
 
-    
 
 if __name__ == '__main__':
     main()
--- Radix.py
+++ Radix.py
@@ -22,6 +22,7 @@ class RadixNode:
         """
         return self.key
 
+
 class RadixTree:
     def __init__(self):
         """
@@ -73,30 +74,80 @@ class RadixTree:
                 return self.search(x.children[a[0]], k[len(a[0]):])
         return False
 
-    def printTree(self,L,i):
+    def print_tree(self, x, string):
         """
         Print the complete sorted tree
-        :param L: list of keys inserted in the tree
-        :param i: position of the string
-        :return: List
+        :param x: node at which leaves are searched for
+        :param string: contains the string that is formed by parent nodes of x
+        :return: None
         """
-        if len(L) <= 1:
-            return L
-        final = []
-        buckets = [ [] for x in range(27) ] # one for each letter in a-z
-        for s in L:
-            if i >= len(s):
-                final.append(s)
-            else:
-                buckets[ ord(s[i]) - ord('a') ].append(s)
-        buckets = [ self.printTree(b, i + 1) for b in buckets ]
-        return final + [ b for blist in buckets for b in blist ]
+        for a in sorted(x.children.keys()):
+            if x.children[a].isLeaf:
+                print(string + a)
+            self.print_tree(x.children[a], string + a)
+
+    def get_last_node(self, x, k):
+        """
+        Returns the last node at which a string terminates
+        :param x: node at which specified string is to be searched for
+        :param k: string which is to be searched for at the node
+        :return: RadixNode
+        """
+        if k == '':
+            return x
+        for a in getAllStrings(k):
+            if a[0] in x.children.keys():
+                return self.get_last_node(x.children[a[0]], k[len(a[0]):])
+        return None
+
+    def print_suggestions(self, string):
+        """
+        Prints all possible words that can be made out of a string
+        :param string: the string whose suggestions are to be found
+        :return: None
+        """
+        node = self.get_last_node(self.root, string)
+        if node:
+            self.print_tree(node, string)
+        else:
+            print("Not a valid string for suggestions")
+
+    def spell_checker(self, string):
+        """
+        Checks is a string exists in the tree
+        :param string: string whose validity is to be checked
+        :return: Boolean
+        """
+        return self.search(self.root, string)
+
 
 def getAllStrings(string):
     """
     Yield all string instances from index 0, decreasing string length by 1 at each iteration
     :param string: the string whose instances are to be found
-    :return: Tuple of strings
+    :return: Tuple
     """
     for a in range(len(string), 0, -1):
         yield (string[:a], string)
+
+
+def main():
+    R = RadixTree()
+    f = open('small.dict')
+    words = f.readlines()
+    words = [line[:-1] for line in words]
+    for a in words:
+        R.insert(R.root, a)
+    print(R.root.children["p"].children["r"].children["o"].children)
+    R.print_suggestions("prq")
+    R.print_suggestions("prob")
+    w = input("Enter a word to check its validity: ")
+    if R.search(R.root, w):
+        print("Correct spelling")
+    else:
+        print("Incorrect spelling")
+
+
+if __name__ == '__main__':
+    main()
+
